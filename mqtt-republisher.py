13bc9390b67d131fd2b159632e609658ed4812a9
Kyle Gordon <kyle@lodge.glasgownet.com>
2012-09-25 00:24:08 +0100

    Moved to python logging module

--- mqtt-republisher.py
+++ mqtt-republisher.py
@@ -8,40 +8,59 @@ import mosquitto
 import os
 import time
 import csv
+import logging
 
 MQTT_HOST="localhost"
 MQTT_PORT=1883
 MQTT_TOPIC="/raw/#"
-mapfile='/etc/mqtt-republisher/map.csv'
+MAPFILE='/etc/mqtt-republisher/map.csv'
+LOGFILE='/var/log/mqtt-republisher.log'
+DEBUG=1
 
 mypid = os.getpid()
 client_uniq = "Republisher_"+str(mypid)
 mqttc = mosquitto.Mosquitto(client_uniq)
 
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL}
+
+if DEBUG == 0: logging.basicConfig(filename=LOGFILE,level=logging.INFO)
+if DEBUG == 1: logging.basicConfig(filename=LOGFILE,level=logging.DEBUG)
+
+logging.info('Starting mqtt-republisher')
+logging.info('INFO MODE')
+logging.debug('DEBUG MODE')
+
 def cleanup():
-    print "Disconnecting"
+    logging.info("Disconnecting from broker")
     mqttc.disconnect()
 
 # Turn the mapping file into a dictionary for internal use
 # Valid from Python 2.7.1 onwards
-with open(mapfile, mode='r') as inputfile:
+with open(MAPFILE, mode='r') as inputfile:
     reader = csv.reader(inputfile)
     mydict = dict((rows[0],rows[1]) for rows in reader)
 
 #define what happens after connection
 def on_connect(rc):
-        print "Connected"
+	logging.info("Connected to broker")
 
 #On recipt of a message print it
 def on_message(msg):
+	logging.debug("Received: " + msg.topic)
 	# print "Received", msg.topic, msg.payload
 	if msg.topic in mydict:
 		## Found an item. Replace it with one from the dictionary
 		# print "Replacing " + msg.topic + " with " + mydict[msg.topic]
 		mqttc.publish(mydict[msg.topic], msg.payload)
+		logging.debug("Republishing: " + msg.topic + " -> " + mydict[msg.topic])
 	else:
 		# Recieved something with a /raw/ topic, but it didn't match. Push it out with /unsorted/ prepended
 		mqttc.publish("/unsorted" + msg.topic, msg.payload)
+		logging.debug("Unknown: " + msg.topic)
 
 try:
 	#connect to broker
@@ -58,8 +77,8 @@ try:
 		pass
 
 except (KeyboardInterrupt):
-    print "Keyboard interrupt received."
+    logging.info("Keyboard interrupt received")
     cleanup()
 except (RuntimeError):
-    print "Crashed for some reason."
+    logging.info("Program crashed")
     cleanup()
