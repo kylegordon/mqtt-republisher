d4217db556cfe8a416072a0ea5a4dce5ca3d3fdb
Kyle Gordon <kyle@lodge.glasgownet.com>
2012-09-28 21:23:20 +0100

    Read the port, not the host

--- mqtt-republisher.py
+++ mqtt-republisher.py
@@ -24,7 +24,7 @@ DEBUG = config.getboolean("global", "debug")
 LOGFILE = config.get("global", "logfile")
 MAPFILE = config.get("global", "mapfile")
 MQTT_HOST = config.get("global", "mqtt_host")
-MQTT_PORT = config.get("global", "mqtt_host")
+MQTT_PORT = config.get("global", "mqtt_port")
 
 client_id = "Republisher_%d" % os.getpid()
 mqttc = mosquitto.Mosquitto(client_id)
